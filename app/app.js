a413be0a9f0ca3997dc3b0054ad080e89b84b093
dksl2 <pcb0213@gmail.com>
2022-04-11 15:33:15 +0900

    프론트단 javascript파일 만들

--- app/app.js
+++ app/app.js
@@ -5,12 +5,13 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./src/routes/home")
-
-app.use("/",home); //use = 미들 웨어 등록해주는 매소드
+const home = require("./src/routes/home");
 
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use("/",home); //use = 미들 웨어 등록해주는 매소드
 
 module.exports = app;
