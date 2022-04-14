b8ba72dda6530720a37d4309c8ea7ee8d10f20a7
dksl2 <pcb0213@gmail.com>
2022-04-14 14:55:38 +0900

    백엔드에서 로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,7 @@ const loginbtn = document.querySelector("button");
                 "Content-Type" : "application/json"
             },
             body : JSON.stringify(req),
-        });
+        })
+        .then((res) => res.json())
+        .then((res) => console.log(res));
     }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,11 +1,40 @@
-const home = (req,res) => {
-    res.render("home/index");
+"use strict"
+
+const output = {
+    home : (req,res) => {
+        res.render("home/index");
+    },
+    login : (req,res) => {
+        res.render("home/login");
+    },
 };
-const login = (req,res) => {
-    res.render("home/login");
+
+const users ={
+    id : ["aa", "bb","cc"],
+    psword :["11","22","33"]
+}
+
+const process ={
+    login : (req, res) =>{
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success : true,
+                });
+            }
+        }
+        return res.json({
+            success : false,
+            msg : "로그인에 실패했습니다.",
+        });
+    },
 };
 
 module.exports ={
-    home,
-    login
+    output,
+    process
 }
